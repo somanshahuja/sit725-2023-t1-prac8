21823f1a7bc1deb309ef17d49d229befe97f0ed5
stephenliu8964 <dq89_64@hotmail.com>
2023-05-05 15:40:04 +1000

    live 2 socket

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -36,6 +36,11 @@ const addProjectToApp = (project) => {
     });
 }
 
+let socket = io();
+socket.on('number',(msg)=>{
+    console.log('Random Number: ' + msg);
+});
+
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('.modal').modal();
